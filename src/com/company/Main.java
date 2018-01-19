df105c0fc846fad419670c5207a646b9e9da4a60
Zech Heneveld <zechheneveld@Zechs-MacBook-Air.local>
2018-01-19 14:27:36 -0700

    assessment with repeat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        Boolean ask2 = true;
         Boolean ask = true;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to ''GUESS THAT NUMBER!'' Please chose a number between 1-10,000 and enter it below");
+        System.out.println("Welcome to ''GUESS THAT NUMBER!''");
 
         int counterNumber = 0;
 
@@ -18,35 +18,50 @@ public class Main {
         Random randomGuess = new Random();
         int gamesRandom = randomGuess.nextInt(lostNumber);
 
-        while(ask){
-            String inputNumber = sc.nextLine();
+        while(ask2) {
+            System.out.println("Please chose a number between 1-10,000 and enter it below");
 
-            int userNumber = Integer.parseInt(inputNumber);
 
-            for (int i = 0; i < 1; i++) {
+            while (ask) {
+                String inputNumber = sc.nextLine();
 
-                counterNumber++;
+                int userNumber = Integer.parseInt(inputNumber);
 
+                for (int i = 0; i < 1; i++) {
+
+                    counterNumber++;
+
+                }
+
+                if (userNumber > gamesRandom) {
+                    System.out.println("Guess " + counterNumber + ": " + userNumber + " is is high. Please enter a lower number.");
+
+                } else if (userNumber < gamesRandom) {
+                    System.out.println("Guess " + counterNumber + ": " + userNumber + " is to low. Please enter a higher number.");
+
+                } else if (userNumber == gamesRandom) {
+                    System.out.println("NUMBER MATCHES! It took you " + counterNumber + " guesses to find the correct number. Would you like to play again?");
+
+                    if (userNumber < gamesRandom || userNumber > gamesRandom) {
+                        ask = true;
+
+                    } else {
+                        ask = false;
+                    }
+
+
+                }
             }
 
-            if (userNumber > gamesRandom) {
-                System.out.println("Guess " + counterNumber + ": " + userNumber + " is is high. Please enter a lower number.");
+            String inputAnswer = sc.nextLine();
 
-            } else if (userNumber < gamesRandom){
-                System.out.println("Guess " + counterNumber + ": " + userNumber + " is to low. Please enter a higher number.");
 
-            } else if (userNumber == gamesRandom){
-                System.out.println("NUMBER MATCHES! It took you " + counterNumber + " guesses to find the correct number.");
-
-                if (userNumber < gamesRandom || userNumber > gamesRandom){
-                    ask = true;
-
-                } else {
-                    ask = false;
+            if (inputAnswer.equals("yes")){
+                ask = true;
+            } else {
+                ask2 = false;
             }
 
-
-            }
         }
 
     }
